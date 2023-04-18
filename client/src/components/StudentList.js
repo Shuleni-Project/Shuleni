33e542f5c505210d82225713a2791f9fa88a7822
brianwambuamu <mutisobrian210@gmail.com>
2023-04-18 15:02:56 +0300

    working on student display

---
+++ client/src/components/StudentList.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    // Make API call to get list of students and their attendance data
+    fetch('/students')
+      .then(response => {
+        setStudents(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Student List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Attendance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student => (
+            <tr key={student.id}>
+              <td>{student.name}</td>
+              <td>{student.attendance}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default StudentList;
