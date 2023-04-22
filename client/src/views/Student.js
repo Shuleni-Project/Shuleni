f877cc76162221019066675224c3c27b18e028b8
Samueelx <gsammygitau@gmail.com>
2023-04-22 23:36:57 +0300

    redisign pie chart.

--- client/src/views/Student.js
+++ client/src/views/Student.js
@@ -4,6 +4,7 @@ import SideNavbar from '../components/SideNavbar';
 import EnrolledCourses from '../components/EnrolledCourses';
 import Chart from '../components/Chart';
 import { studentScores } from '../Data';
+import StudentAttendance from '../components/StudentAttendance';
 
 function Student() {
   const [studentScore, setScore] = useState({
@@ -13,6 +14,13 @@ function Student() {
       data: studentScores.map(data => data.score)
     }]
   });
+  const [attendance, setAttendance] = useState({
+    labels: ['Present', 'Absent'],
+    datasets: [{
+      label: "Student's attendance",
+      data: [75, 25]
+    }]
+  });
   return (
     <div>
       <SideNavbar />
@@ -20,8 +28,13 @@ function Student() {
         <div className="ml-24">
           <h2 className="font-mono text-2xl antialiased font-bold text-sky-900">My Courses</h2>
           <EnrolledCourses />
-          <div className='max-w-sm'>
-            <Chart chartData={studentScore} />
+          <div className='flex flex-wrap justify-around'>
+            <div className='max-w-sm flex-auto'>
+              <Chart chartData={studentScore} />
+            </div>
+            <div className='max-w-sm flex-auto'>
+              <StudentAttendance chartData={attendance} />
+            </div>
           </div>
         </div>
       </div>
